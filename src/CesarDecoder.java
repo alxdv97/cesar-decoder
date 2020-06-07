8dd3a11643812eb6b14888b0eb5208bb9a3d1a88
alxdv97 <alexey.deyev@gmail.com>
2020-06-07 22:51:33 +0300

    remove unnecessary condition and add some comments

--- src/CesarDecoder.java
+++ src/CesarDecoder.java
@@ -1,5 +1,5 @@
 public class CesarDecoder {
-    private static final String encryptedText = "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв" +
+    private static final String ENCRYPTED_TEXT = "Еъёчхф Вхзёюлх, адздёиу ф ждэщхб, црбх еёдюэчъщъгюъв" +
             " южаижжзчх, ждчъёнъгжзчдв. Ъы зёюивй жёхчгюв бюнс ж ъы " +
             "вдгивъгзхбсгрв аёхкдв. Зъеъёс вгъ дмъчющгд, мзд гъюэцъьгджзс" +
             " тздшд аёхкх фчбфъзжф жбъщжзчюъв гъждчъёнъгжзчх " + "мъбдчъмъжадшд югщючющиивх. Ф юэимюб" +
@@ -14,11 +14,11 @@ public class CesarDecoder {
         char[] charArrayEncoded = text.toCharArray();
         char[] charArrayDecoded = new char[charArrayEncoded.length];
         for (int j = 0; j < charArrayEncoded.length; j++) {
-            if (charArrayEncoded[j] == 32) {
+            if (charArrayEncoded[j] == 32) {                    // space
                 charArrayDecoded[j] = charArrayEncoded[j];
-            } else if (charArrayEncoded[j] == 46){
+            } else if (charArrayEncoded[j] == 46) {             // dot
                 charArrayDecoded[j] = charArrayEncoded[j];
-            } else if (charArrayEncoded[j] == 44){
+            } else if (charArrayEncoded[j] == 44) {             // comma
                 charArrayDecoded[j] = charArrayEncoded[j];
             } else if (charArrayEncoded[j] == 'я') {
                 charArrayDecoded[j] = 'а';
@@ -28,27 +28,12 @@ public class CesarDecoder {
                 charArrayDecoded[j] = 'ж';
             } else if (charArrayEncoded[j] == 'Ё') {
                 charArrayDecoded[j] = 'Ж';
-            }  else if (charArrayEncoded[j] == 'е') {
+            } else if (charArrayEncoded[j] == 'е') {
                 charArrayDecoded[j] = 'ё';
             } else if (charArrayEncoded[j] == 'Е') {
                 charArrayDecoded[j] = 'Ё';
-            } else if (charArrayEncoded[j] == 'ъ') {
-                charArrayDecoded[j] = 'ы';
-            } else if (charArrayEncoded[j] == 'Ъ') {
-                charArrayDecoded[j] = 'Ы';
-            } else if (charArrayEncoded[j] == 'ь') {
-                charArrayDecoded[j] = 'э';
-            } else if (charArrayEncoded[j] == 'Ь') {
-                charArrayDecoded[j] = 'Э';
-            } else if (charArrayEncoded[j] == 'ы') {
-                charArrayDecoded[j] = 'ь';
-            } else if (charArrayEncoded[j] == 'Ы') {
-                charArrayDecoded[j] = 'Ь';
-            } else if (charArrayEncoded[j] == 'щ') {
-                charArrayDecoded[j] = 'ъ';
-            } else if (charArrayEncoded[j] == 'Щ') {
-                charArrayDecoded[j] = 'Ъ';
-            } else {
+            }
+            else {
                 charArrayDecoded[j] = (char) (charArrayEncoded[j] + 1);
             }
         }
@@ -58,7 +43,7 @@ public class CesarDecoder {
 
     public static void main(String[] args) {
         CesarDecoder decoder = new CesarDecoder();
-        String probablyDecoded = encryptedText;
+        String probablyDecoded = ENCRYPTED_TEXT;
         System.out.println(probablyDecoded);
         for (int i = 0; i < 33; i++) {
             probablyDecoded = decoder.decode(probablyDecoded);
